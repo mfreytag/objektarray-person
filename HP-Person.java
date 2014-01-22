6e6669b250359acd1116b3fae8577de4feec8d19
mfreytag <mfreytag@gmail.com>
2014-01-22 12:58:06 +0100

    Create HP-Person.java

---
+++ HP-Person.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class HP-Person
+{
+  public static void main(String[] args)
+  {
+  //Aufgabe a - Objektarray
+  final int ANZ = 4;
+  Person arrayPerson[] = new Person[ANZ];
+  for (int i = 0; i < ANZ; i++)
+  {
+    arrayPerson[i] = new Person("Mustermann");
+  }
+  //Ausgabe des Objektarrays
+  for (int i = 0; i < ANZ; i++)
+  {
+    System.out.println(arrayPerson[i]);
+  }
+  //Aufgabe b - ArrayList
+  
+  
+  //Aufgabe c - ersetze letzte Elemente mit "Mustermensch"
+  
+  //Ausgabe der Arrays
+  
+  //Aufgabe d - loesche 3. Element
+  
+  //Angabe der Elementanzahl
+  }
+}
